053deaf365ee0feb6aade3bca68fa6b1421fa80e
JRoberto <kanon939@hotmail.com>
2023-10-02 18:32:25 -0700

    update

--- main.js
+++ main.js
@@ -22,3 +22,60 @@ document.addEventListener("DOMContentLoaded", function () {
 /* Efecto COVER  ##################  */
 
 
+/* Entrada de texto  */
+document.addEventListener("DOMContentLoaded", function() {
+  const description = document.querySelector(".description");
+
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function showElement() {
+    if (isElementInViewport(description)) {
+      description.style.opacity = "1";
+      description.style.transform = "translateY(0)";
+      window.removeEventListener("scroll", checkAndShowElement);
+    }
+  }
+
+  function checkAndShowElement() {
+    if (isElementInViewport(description)) {
+      showElement();
+    }
+  }
+
+  window.addEventListener("scroll", checkAndShowElement);
+});
+
+/*2do texto*/
+
+/*2do texto */
+/* Entrada de texto */
+
+/*Revelado de imagenes */
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".imgRevealed img");
+
+  function checkImages() {
+    images.forEach((image) => {
+      const rect = image.getBoundingClientRect();
+
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        image.classList.add("in-viewport");
+      } else {
+        image.classList.remove("in-viewport");
+      }
+    });
+  }
+
+  // Ejecutar la función al cargar la página y en cada desplazamiento
+  window.addEventListener("scroll", checkImages);
+  checkImages();
+});
+/*Revelado de imagenes  */ 

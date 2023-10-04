10f1f668fbd863a8c77f72e3009903971cb3d362
JRoberto <kanon939@hotmail.com>
2023-10-04 00:18:26 -0700

    update

---
+++ imgArray.js
@@ -0,0 +1,8 @@
+const imagenes = [
+    'imgs/empresa-01.jpg',
+    'imgs/empresa-02.jpg',
+    'imgs/empresa-03.jpg',
+    'imgs/empresa-04.jpg',
+    'imgs/empresa-05.jpg',
+    'imgs/empresa-06.jpg'
+  ];

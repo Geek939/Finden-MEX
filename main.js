f768385b2e7300eb40a1373139e7ac21b5ef4971
JRoberto <kanon939@hotmail.com>
2023-10-04 23:13:33 -0700

    efecto fade

--- main.js
+++ main.js
@@ -81,3 +81,17 @@ document.addEventListener("DOMContentLoaded", function () {
 /*Revelado de imagenes  */ 
 
 /*animacion background section*/
+window.addEventListener('scroll', function()  {
+  let elements = document.getElementsByClassName('fade');
+  let screenSize = window.innerHeight;
+  
+    for(var i = 0; i < elements.length; i++) {
+      let element = elements[i];
+
+      if(element.getBoundingClientRect().top < screenSize) {
+        element.classList.add('visible');
+      } else {
+        element.classList.remove('visible');
+      }
+    }
+});
